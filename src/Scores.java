ef484ddcf5227d61373b91232ad1d8590c0d9912
0wen543 <gensemerob22@gcc.edu>
2024-03-24 14:08:14 -0400

    Update

--- src/Scores.java
+++ src/Scores.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Scores {
     private final ArrayList<Integer> nums;
@@ -11,23 +11,41 @@ public class Scores {
      * @throws IllegalArgumentException if numbers is not a whitespace-delimited list of zero or more integers.
      */
     public Scores(String numbers) throws IllegalArgumentException {
+        Scanner scnr = new Scanner(numbers);
+        scnr.useDelimiter(" ");
         nums = new ArrayList<>();
+        for(int i=0; i<numbers.length(); i++){
+            nums.add(scnr.nextInt());
+        }
     }
 
     /**
      * @return the number of scores in this object i.e. the number of integers in the String given to the constructor
      */
-    public int getNumScores() { return -1; }
+    public int getNumScores() { return nums.size(); }
 
     /**
      * @param index An integer index in the range [0, getNumScores())
      * @return The score at the specified index. For example, the first score in the String is at index 0.
      */
-    public int get(int index) { return -1; }
+    public int get(int index) { return nums.get(index); }
 
     /**
      * @return the maximum score stored in this object
      * @throws java.util.NoSuchElementException if there are no scores
      */
-    public int getMax() throws NoSuchElementException { return -1; }
+    public int getMax() throws NoSuchElementException {
+        int maxNum=0;
+
+        if(nums.isEmpty()){
+            throw new NoSuchElementException("The scores are empty");
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(maxNum<nums.get(i)){
+                maxNum=nums.get(i);
+            }
+        }
+        return maxNum;
+    }
 }
--- src/ScoresTest.java
+++ src/ScoresTest.java
@@ -1,16 +1,105 @@
+import org.junit.jupiter.api.Test;
+
 import java.util.NoSuchElementException;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ScoresTest {
     /*
      * Constructor tests using numScores() and get()
      */
+    @Test
+    public void doubleTest(){
+        final String doubles = "0.45 22.22 3.67";
+        int exceptCount=0;
+        try{
+        Scores aTest = new Scores(doubles);
+        }catch (IllegalArgumentException e){
+        exceptCount++;
+        }
+        assertEquals(1,exceptCount);
+    }
+
+    @Test
+    public void negativeTest(){
+        final String negatives ="-4 -25 -87";
+
+        Scores bTest = new Scores(negatives);
+        assertEquals(3,bTest.getNumScores());
+    }
+
+    @Test
+    public void nothingTest(){
+        final String nothing = "";
+
+        Scores cTest = new Scores(nothing);
+        assertEquals(0,cTest.getNumScores());
+    }
+
+    @Test
+    public void zeroTest(){
+        final String zeros = "0 0 0 0 0 0 0 0";
+
+        Scores dTest = new Scores(zeros);
+        assertEquals(0,dTest.getNumScores());
+    }
+
+    @Test
+    public void bigTest(){
+        final String noSpaces = "43568346176437014389690";
+
+        Scores eTest = new Scores(noSpaces);
+        assertEquals(1,eTest.getNumScores());
+    }
+
+    @Test
+    public void outOfBoundTest(){
+        final String outOfBound = "2 3";
+        int exceptCount=0;
+        Scores fTest = new Scores(outOfBound);
+        try{
+            fTest.get(8);
+        }catch(ArrayIndexOutOfBoundsException e){
+            exceptCount++;
+        }
+        try{
+            fTest.get(-1);
+        }catch (ArrayIndexOutOfBoundsException e){
+            exceptCount++;
+        }
+        assertEquals(2,exceptCount);
+    }
+
 
     /*
      * getMax() tests
      */
+    @Test
+    public void maxTest(){
+        final String same ="4 4 4 4";
 
+        Scores gTest = new Scores(same);
+        assertEquals(4,gTest.getMax());
+    }
+
+    @Test
+    public void negativeMaxTest(){
+        final String negatives="-32 4 6 7";
+
+        Scores hTest = new Scores(negatives);
+        assertEquals(7,hTest.getMax());
+    }
+
+    @Test
+    public void emptyTest(){
+        final String blank=" ";
+        int exceptCount=0;
+        Scores iTest = new Scores(blank);
+        try{
+            iTest.getMax();
+        }catch (NoSuchElementException e){
+            exceptCount++;
+        }
+        assertEquals(1,exceptCount);
+    }
 }
